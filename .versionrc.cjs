38531f89af4dac1947e38d30ca82ec78593991e1
Patrick Kollitsch <patrick@davids-neighbour.com>
2024-06-02 13:00:42 +0700

    chore(fix): remove unused GoHugo partials from release scripts
    Signed-off-by: Patrick Kollitsch <patrick@davids-neighbour.com>

--- .versionrc.cjs
+++ .versionrc.cjs
@@ -4,7 +4,7 @@ const defaultStandardVersion = require('@davidsneighbour/release-config');
 // update version in fetch configuration
 const tracker = {
   filename: "config.schema.json",
-  updater: require("./helpers/schema-version-updater"),
+  updater: require("./helpers/schema-version-updater.cjs"),
 };
 
 const localStandardVersion = {
