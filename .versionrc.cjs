9e2644737f47bdc9b440b81614ee88e9b105a759
Patrick Kollitsch <patrick@davids-neighbour.com>
2024-06-03 14:47:52 +0700

    build(fix): update version updaters
    Signed-off-by: Patrick Kollitsch <patrick@davids-neighbour.com>

--- .versionrc.cjs
+++ .versionrc.cjs
@@ -1,23 +1,23 @@
 const defaultStandardVersion = require('@davidsneighbour/release-config');
 
-
-// update version in fetch configuration
 const tracker = {
-  filename: "config.schema.json",
-  updater: require("./helpers/schema-version-updater.cjs"),
+  filename: 'config.schema.json',
+  updater: require('./helpers/versioning/schema-updater.cjs'),
 };
 
 const localStandardVersion = {
   bumpFiles: [
     {
-      filename: "package.json",
-      type: "json",
+      filename: 'package.json',
+      type: 'json',
     },
     tracker,
   ],
 };
+
 const standardVersion = {
   ...defaultStandardVersion,
   ...localStandardVersion,
 };
+
 module.exports = standardVersion;
--- helpers/versioning/schema-updater.cjs
+++ helpers/versioning/schema-updater.cjs
@@ -1,7 +1,6 @@
-// commit-and-tag-version-updater.js
-const stringifyPackage = require("stringify-package");
-const detectIndent = require("detect-indent");
-const detectNewline = require("detect-newline");
+const stringifyPackage = require('stringify-package');
+const detectIndent = require('detect-indent');
+const detectNewline = require('detect-newline');
 
 module.exports.readVersion = function (contents) {
   return JSON.parse(contents).properties.userAgent.default;
@@ -11,6 +10,6 @@ module.exports.writeVersion = function (contents, version) {
   const json = JSON.parse(contents);
   let indent = detectIndent(contents).indent;
   let newline = detectNewline(contents);
-  json.properties.userAgent.default = "posterboy/v" + version;
+  json.properties.userAgent.default = 'posterboy/v' + version;
   return stringifyPackage(json, indent, newline);
 };
